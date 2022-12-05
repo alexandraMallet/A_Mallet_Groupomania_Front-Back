af26e3638c8f320770e5d955cfb04d210f48ca7e
Alexandra Mallet <malletalexandra@gmail.com>
2022-12-05 14:58:19 +0100

    correction form

--- A_Mallet_Groupomania_frontend/src/views/ModifyUserView.vue
+++ A_Mallet_Groupomania_frontend/src/views/ModifyUserView.vue
@@ -20,7 +20,7 @@
 
             <div class="image-form">
                 <input type="file" id="image" class="image-input" name="image" accept="image/png, image/jpeg"
-                    @change="handleFileUpload($event)" required />
+                    @change="handleFileUpload($event)"/>
                 <label class="image" for="image">Modifier votre photo de profil</label>
 
                 <div class="preview-image">
